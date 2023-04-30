e838d83e021c1240fda86d9fa6cee12148ca25c3
Muhammed Dilmaç <muhammeddilmacc@gmail.com>
2023-04-30 10:22:19 +0300

    Use state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,30 @@
 
+import React, { useState } from 'react';
+
+
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 
 
 export default function ExpenseItem(props) {
-    return (
+    
+    const [title, setTitle] = useState(props.title);
 
+    const changeTitle = () => {
+        setTitle('Updated!');
+    }
+
+    return (
         <Card className='expense-item'>
             <div>
                 <ExpenseDate date={props.date} />
             </div>
             <div className='expense-item__description'>
-                <h2> {props.title} </h2>
+                <h2> {title} </h2>
                 <div className='expense-item__price'> ${props.amount}  </div>
             </div>
-
+            <button onClick={changeTitle}>Change title</button>
         </Card>
-
-
-
     )
 }
